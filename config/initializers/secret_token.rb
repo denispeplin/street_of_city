ab8a0afa5847e2da94f48c669ded949375b425cb
denispeplin <denis.peplin@gmail.com>
2012-08-24 12:10:03 +0400

    Generate new Rails 3.2.8 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreetOfCity::Application.config.secret_token = '071810bba68014d4fa432a2e1239f4d213981bcfba1dac24dd1cc8a2e3e8ffff296dc5fd41b9a9bc9c836921548b80fda20eeb141fdec7fe2fe9a5d2d261ef9e'
